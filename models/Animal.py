e2544b29fc8d436ca2ebf275d859e969428663b9
Booker-AJ <aj_booker@outlook.com>
2022-05-10 21:41:57 -0600

    add methods

--- models/Animal.py
+++ models/Animal.py
@@ -6,4 +6,10 @@ class Animal:
     name: str
     sex: Sex
 
-    
+    def __init__(self, age, name, sex):
+        self.age = age
+        self.name = name
+        self.sex = sex
+
+    def speak(self):
+        print('skwaa')
